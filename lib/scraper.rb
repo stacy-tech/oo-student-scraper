a6ae15da8e884cde90934a0b9c2ce157ce5c6d79
stacy <stacydn23@gmail.com>
2020-11-29 01:30:18 -0500

    commit tree 38236018e99f93a160812622eb109b378404eb90

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,11 +4,44 @@ require 'pry'
 class Scraper
 
   def self.scrape_index_page(index_url)
+    students_hashes = []
+    docs = Nokogiri::HTML(open(index_url))
+    students_infos = docs.css('.student-card')
+    students_infos.each do |student|
+      name=student.css('h4').text
+      location=student.css('p').text
+      profile_url=student.css('a')[0]["href"]
+      student_hash={name:name,location:location,profile_url:profile_url}
+      students_hashes.push(student_hash)
+    end
+    students_hashes
     
   end
 
   def self.scrape_profile_page(profile_url)
-    
+    docs = Nokogiri::HTML(open(profile_url))
+
+    profile_name = docs.css('.profile-name').text
+
+    hash={}
+
+    docs.css('.social-icon-container a').each do |link|
+      if link["href"].include?("twitter")
+        hash[:twitter]=link["href"]
+      elsif link["href"].include?("github")
+        hash[:github]=link["href"]
+      elsif link["href"].include?("linkedin")
+        hash[:linkedin]=link["href"]
+      else
+        hash[:blog]=link["href"]
+      end
+    end
+
+    profile_quote=docs.css('.profile-quote').text
+    bio=docs.css('.description-holder p').text
+    hash[:profile_quote]=profile_quote
+    hash[:bio]=bio
+    hash
   end
 
 end
